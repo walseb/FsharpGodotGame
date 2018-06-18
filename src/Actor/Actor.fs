b6afa8d1f7da853fe7bb00d18beb66d4f9a6ff9c
Sebastian Wålinder <s.walinder@gmail.com>
2018-06-18 15:52:14 +0200

    Worked on actor state machine

--- src/Actor/Actor.fs
+++ src/Actor/Actor.fs
@@ -9,117 +9,102 @@ open InputManager.PlayerInputActions
 module StateMachine =
     type ActorState =
     | CivilState
-    | HoldWeaponState
-    | HolsterState
     | UnHolsterState
+    | HolsterState
+    | HoldWeaponState
+
+    module Actions =
+        let Move (stateMultiplier : float32) =
+            let getMoveAxis() : Vector2 =
+                Vector2(0.0f, 0.0f)
+                |> fun axis -> if Input.IsActionPressed PlayerInputActions.moveUp then Vector2(axis.x + 1.0f, axis.y) else axis
+                |> fun axis -> if Input.IsActionPressed PlayerInputActions.moveDown then Vector2(axis.x - 1.0f, axis.y)  else axis
+                |> fun axis -> if Input.IsActionPressed PlayerInputActions.moveLeft then Vector2(axis.x, axis.y + 1.0f) else axis
+                |> fun axis -> if Input.IsActionPressed PlayerInputActions.moveRight then Vector2(axis.x, axis.y - 1.0f) else axis
+
+            getMoveAxis()
+            |> fun moveDir -> if Input.IsActionPressed PlayerInputActions.sprint then moveDir * 1.25f else moveDir
+            |>  fun moveDir -> moveDir * stateMultiplier
+
+        let Pickup() =
+            if Input.IsActionPressed PlayerInputActions.pickup then GD.Print "Pickup isn't implemented yet"
+
+        let Aim() =
+            if Input.IsActionPressed PlayerInputActions.pickup then GD.Print "Aim isn't implemented yet"
 
     module UpdateCivilState =
-        let mutable pressedMoveUp : bool = false
-        let mutable pressedMoveDown : bool = false
-        let mutable pressedMoveLeft : bool = false
-        let mutable pressedMoveRight : bool = false
+        let mutable moveDirection = Vector2(0.0f, 0.0f)
 
-        let mutable pressedPickup : bool = false
-        let mutable pressedSprint : bool = false
+        let UpdateState() : ActorState =
+            moveDirection <- Actions.Move 1.0f
+            Actions.Pickup()
 
-        let mutable pressedAttack : bool = false
-        let mutable pressedAim : bool = false
+            if Input.IsActionPressed PlayerInputActions.aim
+            then UnHolsterState
+            else CivilState
 
-        let UpdateCivilState() =
-            1
-
-        let PhysicsUpdateCivilState() =
-            1
-
-        let InputUpdateCivilState(inputEvent : InputEvent) =
-            match inputEvent :? InputEventKey with
-            | true ->
-                let inputEventKey = inputEvent :?> InputEventKey
-
-                let updateKeyState pressed (scancode : string) =
-                    match scancode with
-                        | moveUp ->
-                            pressedMoveUp <- pressed
-                        | moveDown ->
-                            pressedMoveDown <- pressed
-                        | moveLeft ->
-                            pressedMoveLeft <- pressed
-                        | moveRight ->
-                            pressedMoveRight <- pressed
-                        | pickup ->
-                            pressedPickup <- pressed
-                        | sprint ->
-                            pressedSprint <- pressed
-                        | attack ->
-                            pressedAttack <- pressed
-                        | aim ->
-                            pressedAim <- pressed
-
-                // If pressed signal, or released signal
-                match inputEventKey.Pressed with
-                    | true ->
-                        OS.GetScancodeString(inputEventKey.Scancode)
-                        |> updateKeyState true
-                    | false ->
-                        OS.GetScancodeString(inputEventKey.Scancode)
-                        |> updateKeyState false
-            // Is not key, fix later
-            | false ->
-                ()
-            ()
+        let PhysicsUpdateState(node : KinematicBody) =
+            node.MoveAndSlide(Vector3(moveDirection.x, moveDirection.y, 0.0f))
 
     module UpdateHoldWeaponState =
-        let UpdateHoldWeaponState() =
-            1
+        let mutable moveDirection = Vector2(0.0f, 0.0f)
+        let UpdateState() : ActorState =
+            moveDirection <- Actions.Move 1.0f
+            Actions.Pickup()
+            HoldWeaponState
 
-        let InputUpdateHoldWeaponState(inputEvent : InputEvent) =
-            1
+        let PhysicsUpdateState(node : KinematicBody) =
+            node.MoveAndSlide(Vector3(moveDirection.x, moveDirection.y, 0.0f))
 
     module UpdateHolsterState =
-        let UpdateHolsterState() =
-            1
+        let mutable moveDirection = Vector2(0.0f, 0.0f)
+        let UpdateState() : ActorState =
+            moveDirection <- Actions.Move 1.0f
+            HolsterState
 
-        let InputUpdateHolsterState(inputEvent : InputEvent) =
-            1
+        let PhysicsUpdateState(node : KinematicBody) =
+            node.MoveAndSlide(Vector3(moveDirection.x, moveDirection.y, 0.0f))
 
     module UpdateUnHolsterState =
-        let UpdateUnHolsterState() =
-            1
+        let mutable moveDirection = Vector2(0.0f, 0.0f)
+        let UpdateState() : ActorState =
+            moveDirection <- Actions.Move 1.0f
+            UnHolsterState
 
-        let InputUpdateUnHolsterState(inputEvent : InputEvent) =
-            1
+        let PhysicsUpdateState(node : KinematicBody) =
+            node.MoveAndSlide(Vector3(moveDirection.x, moveDirection.y, 0.0f))
 
-    let UpdateStateMachine() (state : ActorState) : ActorState =
-        CivilState
+    let UpdateStateMachine (actorState : ActorState) : ActorState =
+        match actorState with
+            | CivilState -> UpdateCivilState.UpdateState()
+            | UnHolsterState -> UpdateUnHolsterState.UpdateState()
+            | HoldWeaponState -> UpdateHoldWeaponState.UpdateState()
+            | HolsterState -> UpdateHolsterState.UpdateState()
 
-    let PhysicsUpdateStateMachine(state : ActorState) : ActorState =
-        // match state with
-        // | CivilState -> CivilState.PhysicsUpdateCivilState() |> ignore
-        // | _ -> 1 |> ignore
-         CivilState
-
-    let InputUpdateStateMachine(inputEvent : InputEvent) : ActorState =
-        UpdateCivilState.InputUpdateCivilState inputEvent
-        CivilState
-
+    let PhysicsUpdateStateMachine (actorState : ActorState) (kinematicBody : KinematicBody) =
+        match actorState with
+            | CivilState -> UpdateCivilState.PhysicsUpdateState kinematicBody
+            | UnHolsterState -> UpdateUnHolsterState.PhysicsUpdateState kinematicBody
+            | HoldWeaponState -> UpdateHoldWeaponState.PhysicsUpdateState kinematicBody
+            | HolsterState -> UpdateHolsterState.PhysicsUpdateState kinematicBody
 
 type ActorObject() as this =
     inherit Node()
 
-    let mutable state : StateMachine.ActorState =
-        StateMachine.CivilState
-
+    let mutable state = StateMachine.ActorState.CivilState
 
     override this._Ready() =
         this.SetProcessInput true
 
-
     override this._Process(delta : float32) =
-        1 |> ignore
-
-    override this._Input(inputEvent : InputEvent) =
-        this.GetTree().SetInputAsHandled();
-        StateMachine.InputUpdateStateMachine inputEvent |> ignore
+        StateMachine.UpdateStateMachine state
         ()
+
+    override this._PhysicsProcess(delta : float32) =
+        StateMachine.PhysicsUpdateStateMachine state
+        ()
+    // override this._Input(inputEvent : InputEvent) =
+        // this.GetTree().SetInputAsHandled();
+        // ()
 
         //state <- StateMachine.InputUpdateStateMachine inputEvent
